236238f0ff1b51ca5a628609c4674e0385170cb3
Alexander Herrmann <darignac@gmail.com>
2016-03-29 20:47:57 +0200

    added utmost header line

--- header.php
+++ header.php
@@ -12,9 +12,29 @@
 	<div class="container">
 		<header>
 			<?php do_action( 'header_before' ); ?>
+			<div class="row top">
+				<?php
+					$cssHeadline = 'col-sm-10 col-xs-10';
+				?>
+				<?php if ( get_theme_mod( 'logo_upload' ) ): $cssHeadline = 'col-sm-9 hidden-xs-down'; ?>
+					<div class="col-sm-1 col-xs-10">
+						<img src="<?php echo esc_url( get_theme_mod( 'logo_upload' ) ); ?>" alt="<?php echo get_bloginfo( 'name' ); ?>" />
+					</div>
+				<?php endif; ?>
+				<div class="<?php echo $cssHeadline; ?>">
+					<h1>
+						<a href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ) ;?>">
+							<?php echo get_bloginfo( 'name' ); ?>
+						</a>
+					</h1>
+				</div>
+				<div class="col-sm-2 col-xs-2">
+					<button class="navbar-toggler pull-sm-right pull-xs-right" type="button" data-toggle="collapse" data-target="#collapsingNavbar">&#9776;</button>
+				</div>
+			</div>
 			<?php get_template_part( 'menu/primary' ); ?>
 			<div class="row">
-				<div class="col-sm-12">TODO WIDGETS</div>
+				<div class="col-sm-12"><!-- TODO WIDGETS --></div>
 			</div>
 			<?php do_action( 'header_after' ); ?>
 		</header>
