1fc397cf488881dad829a36744c227865e6f2775
Alexander Herrmann <darignac@gmail.com>
2016-03-30 19:47:06 +0200

    adjusted header orientations

--- header.php
+++ header.php
@@ -12,16 +12,16 @@
 	<div class="container">
 		<header>
 			<?php do_action( 'header_before' ); ?>
-			<div class="row top">
+			<div class="row topline">
 				<?php
 					$cssHeadline = 'col-sm-10 col-xs-10';
 				?>
 				<?php if ( get_theme_mod( 'logo_upload' ) ): $cssHeadline = 'col-sm-9 hidden-xs-down'; ?>
-					<div class="col-sm-1 col-xs-10">
+					<div class="col-sm-1 col-xs-10 logo">
 						<img src="<?php echo esc_url( get_theme_mod( 'logo_upload' ) ); ?>" alt="<?php echo get_bloginfo( 'name' ); ?>" />
 					</div>
 				<?php endif; ?>
-				<div class="<?php echo $cssHeadline; ?>">
+				<div class="headline <?php echo $cssHeadline; ?>">
 					<h1>
 						<a href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ) ;?>">
 							<?php echo get_bloginfo( 'name' ); ?>
