7dacb035041a3c6402f982daa58a146f95da887d
Alexander Herrmann <darignac@gmail.com>
2016-04-03 15:22:06 +0200

    fixed header structure and removed vertical scrollbar with this

--- header.php
+++ header.php
@@ -11,33 +11,38 @@
 	<div class="container">
 
 		<?php do_action( 'body_top' ); ?>
-		<header>
-			<?php do_action( 'header_before' ); ?>
-			<div class="row topline">
-				<div class="col-sm-10 col-xs-10 media">
-					<?php if ( get_theme_mod( 'logo_upload' ) ): ?>
-						<div class="media-left">
-							<img class="media-object" src="<?php echo esc_url( get_theme_mod( 'logo_upload' ) ); ?>" alt="<?php echo get_bloginfo( 'name' ); ?>" />
+		<header class="row">
+			<div class="col-sm-12">
+
+				<?php do_action( 'header_before' ); ?>
+
+				<div class="row topline">
+					<div class="col-sm-10 col-xs-10 media">
+						<?php if ( get_theme_mod( 'logo_upload' ) ): ?>
+							<div class="media-left">
+								<img class="media-object" src="<?php echo esc_url( get_theme_mod( 'logo_upload' ) ); ?>" alt="<?php echo get_bloginfo( 'name' ); ?>" />
+							</div>
+						<?php endif; ?>
+						<div class="media-body hidden-xs-down">
+							<h1 class="media-heading">
+								<a href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ) ;?>">
+									<?php echo get_bloginfo( 'name' ); ?>
+								</a>
+							</h1>
+							<?php if ( get_bloginfo( 'description' ) ): ?><p><?php echo get_bloginfo( 'description' ); ?></p><?php endif; ?>
 						</div>
-					<?php endif; ?>
-					<div class="media-body hidden-xs-down">
-						<h1 class="media-heading">
-							<a href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ) ;?>">
-								<?php echo get_bloginfo( 'name' ); ?>
-							</a>
-						</h1>
-						<?php if ( get_bloginfo( 'description' ) ): ?><p><?php echo get_bloginfo( 'description' ); ?></p><?php endif; ?>
+					</div>
+					<div class="col-sm-2 col-xs-2">
+						<button class="navbar-toggler pull-sm-right pull-xs-right" type="button" data-toggle="collapse" data-target="#collapsingNavbar">&#9776;</button>
 					</div>
 				</div>
-				<div class="col-sm-2 col-xs-2">
-					<button class="navbar-toggler pull-sm-right pull-xs-right" type="button" data-toggle="collapse" data-target="#collapsingNavbar">&#9776;</button>
-				</div>
+				<?php get_template_part( 'menu/primary' ); ?>
+				<div class="row">
+					<div class="col-sm-12"><!-- TODO WIDGETS --></div>
+					
+				<?php do_action( 'header_after' ); ?>
+
 			</div>
-			<?php get_template_part( 'menu/primary' ); ?>
-			<div class="row">
-				<div class="col-sm-12"><!-- TODO WIDGETS --></div>
-			</div>
-			<?php do_action( 'header_after' ); ?>
 		</header>
 
 		<?php do_action( 'main_before' ); ?>
