555e9b1ecfa48d31295c59cd9db2b3ead7defd34
Alexander Herrmann <darignac@gmail.com>
2016-05-01 17:33:44 +0200

    restructured post view

--- content.php
+++ content.php
@@ -1,14 +1,18 @@
-<div <?php post_class('row'); ?>>
+<article <?php post_class(); ?>>
 	<?php do_action( 'post_before' ); ?>
 
-	<article class="col-sm-12">
-		<h1 class="text-xs-center">
-			<a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a>
-		</h1>
-		<?php get_template_part( 'content/post-meta' ); ?>
-		<?php get_template_part( 'content/featured-image' ); ?>
-		<?php the_excerpt(); ?>
-	</article>
+	<div class="row">
+		<div class="col-sm-10 col-sm-offset-1">
+			<h1 class="text-xs-center">
+				<a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a>
+			</h1>
+			<?php get_template_part( 'content/post-meta' ); ?>
+		</div>
+		<div class="col-sm-12">
+			<?php get_template_part( 'content/post-featured-image' ); ?>
+		</div>
+		<div class="col-sm-10 col-sm-offset-1"><?php the_excerpt(); ?></div>
+	</div>
 
 	<?php do_action( 'post_after' ); ?>
-</div>
+</article>
