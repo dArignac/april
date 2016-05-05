8978b0190e059ab4392bdfd587d76c2a89664ce6
Alexander Herrmann <darignac@gmail.com>
2016-05-05 16:37:00 +0200

    fixed display of readmore and comments

--- content-archive.php
+++ content-archive.php
@@ -18,11 +18,12 @@
 
 		<div class="col-sm-10 col-sm-offset-1"><?php the_excerpt(); ?></div>
 
-		<div class="comments col-sm-10 col-sm-offset-1 text-xs-center">
-			<a class="continue-reading" href="<?php echo esc_url( get_permalink() ); ?>"><?php echo __( 'Continue reading', 'april' ) ?></a>
+		<div class="comments col-sm-5 col-sm-offset-1 text-md-right col-xs-12 text-xs-center">
+			<a class="post-continue-reading" href="<?php echo esc_url( get_permalink() ); ?>"><?php echo __( 'Continue reading', 'april' ) ?></a>
+		</div>
+		<div class="comments col-sm-5 text-md-left col-xs-12 text-xs-center">
 			<?php get_template_part( 'content/post-comments-link' ); ?>
 		</div>
-
 	</div>
 
 	<?php do_action( 'archive_post_after' ); ?>
--- content/post-comments-link.php
+++ content/post-comments-link.php
@@ -1,10 +1,10 @@
-<i class="fa fa-comment" title="<?php _e( 'Comment icon', 'april' ); ?>"></i>
 <?php
 	if ( ! comments_open() && get_comments_number() < 1 ) :
+		echo '<a class="post-comment-count" href="#">';
 		comments_number( __( 'Comments closed', 'april' ), __( 'One Comment', 'april' ), __( '% Comments', 'april' ) );
+		echo '</a>';
 	else :
-		echo '<a href="' . esc_url( get_comments_link() ) . '">';
+		echo '<a class="post-comment-count" href="' . esc_url( get_comments_link() ) . '">';
 		comments_number( __( 'Leave a Comment', 'april' ), __( 'One Comment', 'april' ), __( '% Comments', 'april' ) );
 		echo '</a>';
 	endif;
-?>
