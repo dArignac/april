a1773b9373e8649eb4eacbc800d6f916b392905d
Alexander Herrmann <darignac@gmail.com>
2016-05-05 15:36:59 +0200

    added category listing

--- content.php
+++ content.php
@@ -2,23 +2,29 @@
 	<?php do_action( 'post_before' ); ?>
 
 	<div class="row">
+
 		<div class="col-sm-10 col-sm-offset-1">
 			<h1 class="text-xs-center">
 				<a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a>
 			</h1>
 			<?php get_template_part( 'content/post-meta' ); ?>
 		</div>
+
 		<div class="col-sm-12">
 			<?php get_template_part( 'content/post-featured-image' ); ?>
 		</div>
-		<div class="col-sm-10 col-sm-offset-1">
-			TODO
-		</div>
-		<div class="comments col-sm-10 col-sm-offset-1 text-xs-center">
-			<a class="continue-reading" href="<?php echo esc_url( get_permalink() ); ?>"><?php echo __( 'Continue reading', 'april' ) ?></a>
-			<?php get_template_part( 'content/post-comments-link' ); ?>
-		</div>
-	</div>
 
-	<?php do_action( 'post_after' ); ?>
+		<div class="col-sm-10 col-sm-offset-1">
+			<?php the_content(); ?>
+			<?php wp_link_pages( array(
+				'before' => '<p class="post-pagination">' . __( 'Pages:', 'april' ),
+				'after'  => '</p>',
+			) ); ?>
+		</div>
+
+		<?php do_action( 'post_after' ); ?>
+		
+		<?php get_template_part( 'content/post-categories' ); ?>
+
+	</div>
 </article>
---
+++ content/post-categories.php
@@ -0,0 +1,8 @@
+<?php if ( $categories = get_the_category( $post->ID ) ): ?>
+	<div class="col-sm-10 col-sm-offset-1">
+		<?php echo __( 'Published in:', 'april' ); ?>
+		<?php foreach ($categories as $category): ?>
+			<a href="<?php echo esc_url( get_category_link( $category->term_id ) ); ?>" title="<?php echo esc_attr( sprintf( __( 'View all posts in %s', 'april' ), $category->name ) ) ?>"><?php echo $category->cat_name; ?></a>
+		<?php endforeach; ?>
+	</div>
+<?php endif; ?>
