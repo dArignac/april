c55197a56a9959f293d6a9b1555f6435ec6605de
Alexander Herrmann <darignac@gmail.com>
2016-05-05 15:25:35 +0200

    added some space

--- content-archive.php
+++ content-archive.php
@@ -2,20 +2,25 @@
 	<?php do_action( 'archive_post_before' ); ?>
 
 	<div class="row">
+
 		<div class="col-sm-10 col-sm-offset-1">
 			<h1 class="text-xs-center">
 				<a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a>
 			</h1>
 			<?php get_template_part( 'content/post-meta' ); ?>
 		</div>
+
 		<div class="col-sm-12">
 			<?php get_template_part( 'content/post-featured-image' ); ?>
 		</div>
+
 		<div class="col-sm-10 col-sm-offset-1"><?php the_excerpt(); ?></div>
+
 		<div class="comments col-sm-10 col-sm-offset-1 text-xs-center">
 			<a class="continue-reading" href="<?php echo esc_url( get_permalink() ); ?>"><?php echo __( 'Continue reading', 'april' ) ?></a>
 			<?php get_template_part( 'content/post-comments-link' ); ?>
 		</div>
+
 	</div>
 
 	<?php do_action( 'archive_post_after' ); ?>
