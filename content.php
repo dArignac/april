b11c5d5b41689585e0cf6c1ceb00f7eb575403c1
Alexander Herrmann <darignac@gmail.com>
2016-05-22 15:12:37 +0200

    fixed content styles

--- content.php
+++ content.php
@@ -4,10 +4,10 @@
 	<div class="row">
 
 		<div class="col-sm-10 col-sm-offset-1">
-			<h1>
+			<h1 class="text-xs-center">
 				<a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a>
 			</h1>
-			<h2>
+			<h2 class="text-xs-center">
 				<?php get_template_part( 'content/post-meta' ); ?>
 			</h2>
 		</div>
