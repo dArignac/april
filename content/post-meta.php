29d44f8634d6da88731ceecede386b94e827669f
Alexander Herrmann <darignac@gmail.com>
2019-05-19 11:53:33 +0200

    now really fixed #51

--- content/post-meta.php
+++ content/post-meta.php
@@ -1,6 +1,6 @@
 <?php
 $display_author = get_theme_mod( 'april_display_author' );
-$date   = date_i18n( get_option( 'date_format' ), strtotime( get_the_date( ) ) );
+$date   = date_i18n( get_option( 'date_format' ), get_the_date( 'U' ) );
 $author = get_the_author();
 ?>
 <?php if ( $display_author ): ?>
